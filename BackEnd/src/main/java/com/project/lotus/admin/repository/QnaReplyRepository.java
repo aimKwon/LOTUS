479985e1ee0991f11b41524669e189204cc68505
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-05-28 22:41:32 +0900

    Update QnaReplyRepository.java

--- BackEnd/src/main/java/com/project/lotus/admin/repository/QnaReplyRepository.java
+++ BackEnd/src/main/java/com/project/lotus/admin/repository/QnaReplyRepository.java
@@ -2,7 +2,6 @@ package com.project.lotus.admin.repository;
 
 import com.project.lotus.admin.entity.QnaReply;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
