21a32c3bcd86405ba1a0b453c7cc4a52f60c56b1
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-01 18:18:03 +0900

    Update UserRepository.java

--- BackEnd/src/main/java/com/project/lotus/auth/repository/UserRepository.java
+++ BackEnd/src/main/java/com/project/lotus/auth/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.project.lotus.auth.repository;
 
 import com.project.lotus.auth.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
