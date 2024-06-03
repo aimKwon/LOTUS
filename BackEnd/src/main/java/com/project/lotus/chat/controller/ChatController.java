66982d3e230f1f4dc16882b154b4f08940dd3d96
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-03 20:39:13 +0900

    Update ChatController.java

--- BackEnd/src/main/java/com/project/lotus/chat/controller/ChatController.java
+++ BackEnd/src/main/java/com/project/lotus/chat/controller/ChatController.java
@@ -9,9 +9,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
