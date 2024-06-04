660ce61674f666e0a5b55f19e4488e7822ff54ca
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-04 19:07:19 +0900

    Update RefreshTokenRepository.java

--- BackEnd/src/main/java/com/project/lotus/common/config/security/repository/RefreshTokenRepository.java
+++ BackEnd/src/main/java/com/project/lotus/common/config/security/repository/RefreshTokenRepository.java
@@ -2,7 +2,6 @@ package com.project.lotus.common.config.security.repository;
 
 import com.project.lotus.common.config.security.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
