c119736d6c0a2d94220c482d302ae0923e025431
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-06 18:44:02 +0900

    Update ChatServiceImpl.java

--- BackEnd/src/main/java/com/project/lotus/chat/service/impl/ChatServiceImpl.java
+++ BackEnd/src/main/java/com/project/lotus/chat/service/impl/ChatServiceImpl.java
@@ -13,7 +13,6 @@ import com.project.lotus.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
