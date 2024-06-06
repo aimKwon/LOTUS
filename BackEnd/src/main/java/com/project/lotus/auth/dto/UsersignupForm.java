cd7ed1771dfdfe441cd874957f4b7000004ca62f
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-06 18:42:59 +0900

    Update UsersignupForm.java

--- BackEnd/src/main/java/com/project/lotus/auth/dto/UsersignupForm.java
+++ BackEnd/src/main/java/com/project/lotus/auth/dto/UsersignupForm.java
@@ -1,6 +1,5 @@
 package com.project.lotus.auth.dto;
 
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
