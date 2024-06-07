cd7ecdafe734e4d7c3e4b1a8f2853b410b18e724
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-07 10:51:24 +0900

    Update RefreshTokenController.java

--- BackEnd/src/main/java/com/project/lotus/common/config/security/controller/RefreshTokenController.java
+++ BackEnd/src/main/java/com/project/lotus/common/config/security/controller/RefreshTokenController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
-
 import static org.springframework.http.HttpStatus.OK;
 
 @RequestMapping("/refreshToken")
