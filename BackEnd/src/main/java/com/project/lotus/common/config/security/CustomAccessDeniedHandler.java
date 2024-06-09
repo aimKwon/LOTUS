fec6f6c5b8f3d3d918ab41ca3adf9262481233ee
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-09 18:34:56 +0900

    Update CustomAccessDeniedHandler.java

--- BackEnd/src/main/java/com/project/lotus/common/config/security/CustomAccessDeniedHandler.java
+++ BackEnd/src/main/java/com/project/lotus/common/config/security/CustomAccessDeniedHandler.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
-
 import java.io.IOException;
 
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
