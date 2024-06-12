ecb6906f34723f9948952d4aeadf03e458119f4c
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-12 19:01:19 +0900

    Update CustomAuthenticationEntryPoint.java

--- BackEnd/src/main/java/com/project/lotus/common/config/security/CustomAuthenticationEntryPoint.java
+++ BackEnd/src/main/java/com/project/lotus/common/config/security/CustomAuthenticationEntryPoint.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-
 import java.io.IOException;
 
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
