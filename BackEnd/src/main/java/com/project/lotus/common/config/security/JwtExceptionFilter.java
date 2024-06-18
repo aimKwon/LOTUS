40bcb9dad11d6c774d4bfcbff9be35dd8949fa26
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-18 21:10:07 +0900

    Update JwtExceptionFilter.java

--- BackEnd/src/main/java/com/project/lotus/common/config/security/JwtExceptionFilter.java
+++ BackEnd/src/main/java/com/project/lotus/common/config/security/JwtExceptionFilter.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
 
 @Component
