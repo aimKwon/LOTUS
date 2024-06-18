dd1030bc5c894c483700cdc5d194b7e3be52bd7b
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-18 21:10:21 +0900

    Update PasswordEncoderConfig.java

--- BackEnd/src/main/java/com/project/lotus/common/config/PasswordEncoderConfig.java
+++ BackEnd/src/main/java/com/project/lotus/common/config/PasswordEncoderConfig.java
@@ -1,6 +1,5 @@
 package com.project.lotus.common.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
