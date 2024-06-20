11d00142b8053e1e816ccb66c28351176a34ea7b
Jihyun Kwon <145642491+aimKwon@users.noreply.github.com>
2024-06-21 07:18:43 +0900

    Update ProductDto.java

--- BackEnd/src/main/java/com/project/lotus/product/dto/ProductDto.java
+++ BackEnd/src/main/java/com/project/lotus/product/dto/ProductDto.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
 
 public class ProductDto {
